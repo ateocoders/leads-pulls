84ea5f4515f3bc813ce0cd6b56f016be09014a0e
ateocoders <41297029+ateocoders@users.noreply.github.com>
2018-07-18 12:18:03 +0200

    Update leadapi.php

--- leadapi.php
+++ leadapi.php
@@ -1,12 +1,10 @@
 <?php
 
 ini_set("allow_url_fopen", 1);
-
 $purejsondata = file_get_contents('http://www.reporting.rf.gd/SF/cnx.php');
 $data = json_decode($purejsondata,true);
 
 echo $data["prospects"][0]["ID"];
 echo $data["prospects"][0]["EMAIL"];
-
 
 ?>
