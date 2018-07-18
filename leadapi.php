a83f4d6b91fa6afa6fbd050df0a67f6d25e3f63d
ateocoders <41297029+ateocoders@users.noreply.github.com>
2018-07-18 12:13:02 +0200

    Update leadapi.php

--- leadapi.php
+++ leadapi.php
@@ -2,11 +2,11 @@
 
 ini_set("allow_url_fopen", 1);
 
-$json = file_get_contents('http://localhost/Salesforce/cnx.php');
-$obj = json_decode($json,true);
+$purejsondata = file_get_contents('http://www.reporting.rf.gd/SF/cnx.php');
+$data = json_decode($purejsondata,true);
 
-echo $obj["prospects"][0]["ID"];
-echo $obj["prospects"][0]["EMAIL"];
+echo $data["prospects"][0]["ID"];
+echo $data["prospects"][0]["EMAIL"];
 
 
 ?>
