f5eeedc87482c6ed62863a6f159aae81b815d050
ateocoders <41297029+ateocoders@users.noreply.github.com>
2018-07-18 12:54:18 +0200

    Update leadapi.php

--- leadapi.php
+++ leadapi.php
@@ -1,7 +1,7 @@
 <?php
 
 ini_set("allow_url_fopen", 1);
-$purejsondata = file_get_contents('http://www.reporting.rf.gd/SF/cnx.php');
+$purejsondata = file_get_contents('http://www.reporting.rf.gd/SF/cnx.php?token=YOUR_TOKEN');
 $data = json_decode($purejsondata,true);
 
 echo $data["prospects"][0]["ID"]; //example of retreiving the first lead's id
